661b749e1bed02b0df3e8c37cc1d4ba19758cd36
prakruti <prakrutigotawala30@gmail.com>
2026-05-29 13:01:46 +0530

    rating

--- LibraryManagementSystem.ClassLibrary/Migrations/20260529062009_AddBookReviews.Designer.cs
+++ LibraryManagementSystem.ClassLibrary/Migrations/20260529062009_AddBookReviews.Designer.cs
@@ -4,6 +4,7 @@ using LibraryManagementSystem.ClassLibrary.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManagementSystem.ClassLibrary.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529062009_AddBookReviews")]
+    partial class AddBookReviews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -197,9 +200,6 @@ namespace LibraryManagementSystem.ClassLibrary.Migrations
 
                     b.Property<bool>("IsFeatured")
                         .HasColumnType("bit");
-
-                    b.Property<string>("PdfUrl")
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Title")
                         .IsRequired()
